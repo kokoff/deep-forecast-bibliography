db416e32246c3334c2955f1f4f545fa85c76fa46
skokov <sk1056@york.ac.uk>
2017-11-13 19:41:17 +0000

    Auto commit

---
+++ files/UVDISYDL/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "a-little-book-of-r-for-time-series",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/",
+    theme: "sphinxdoc",
+    builder: "sphinx",
+    docroot: "/./",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "f6d98ef4"
+  };
+  
---
+++ files/WBR3FE8S/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "YORK",
+      'continent': "EU",
+      'country': "GB",
+      'region': "EN"
+    },
+    'ip':"86.191.89.182"
+  }]);
+})
+//
+()
+
+;
